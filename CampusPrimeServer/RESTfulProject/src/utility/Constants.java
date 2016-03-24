4554dcfc6cf6c28547eee85c7bc0ec9517106c09
Linson <jklinson@gmail.com>
2016-03-24 22:35:29 +0530

    register file changes

--- CampusPrimeServer/RESTfulProject/src/utility/Constants.java
+++ CampusPrimeServer/RESTfulProject/src/utility/Constants.java
@@ -7,5 +7,5 @@ public class Constants {
 	public static final String STATUS_FAILURE 	= "Failure";
 	
 	public static final String MESSAGE_KEY		= "message";
-	public static final String FILE_UPLOAP_PATH = "D:/Linson/WorkSpace/WebApps/FileUpload/";
+	public static final String FILE_UPLOAP_PATH = "G:/Mini Project/gitnew/riya/";
 }
